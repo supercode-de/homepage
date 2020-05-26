430f422d7b53d52c3c3323802cfae73bb5fd5020
elainewo <yw.4contact@gmail.com>
2020-05-26 17:07:11 +0200

    workshop.js and HeaderWS.js

--- components/Banner.js
+++ components/Banner.js
@@ -1,33 +1,37 @@
 // import React from 'react';  brauchen wir nicht oder?
-import Link from 'next/link'
+import Link from "next/link";
 const Banner = () => {
-    return (
-        <section id="banner">
-            <h3>Unsere Anmeldephase läuft.<a href="/">erfahre mehr</a> </h3>
-            <style jsx>{`
-            #banner{
-                background:#3DD7AC;
-                text-align:center;
-                padding:10px 0;
-            }
-            #banner h3{
-                margin:0;
-                font-family: 'Fira Mono', monospace;
-                font-style: normal;
-                font-weight: bold;
-                font-size: 16px;
-                line-height: 19px;
-                letter-spacing: 0.2em;
-                text-transform: uppercase;
-                color: #5D3EDE;
-                font-weight:500;
-            }
-            #banner h3 a{
-                color: #5D3EDE;
-            }
-            `}
-            </style>
-        </section>);
-}
+  return (
+    <section id="banner">
+      <h3>
+        Unsere Anmeldephase läuft.<a href="/">erfahre mehr</a>{" "}
+      </h3>
+      <style jsx>
+        {`
+          #banner {
+            background: #3dd7ac;
+            text-align: center;
+            padding: 10px 0;
+          }
+          #banner h3 {
+            margin: 0;
+            font-family: "Fira Mono", monospace;
+            font-style: normal;
+            font-weight: bold;
+            font-size: 16px;
+            line-height: 19px;
+            letter-spacing: 0.2em;
+            text-transform: uppercase;
+            color: #5d3ede;
+            font-weight: 500;
+          }
+          #banner h3 a {
+            color: #5d3ede;
+          }
+        `}
+      </style>
+    </section>
+  );
+};
 
 export default Banner;
---
+++ pages/workshop.js
@@ -0,0 +1,56 @@
+import Head from "next/head";
+import HeaderWS from "../components/HeaderWS";
+import { useWindowSize } from "../components/functions/windowSize";
+
+export default function Workshop() {
+  if (process.browser) {
+    const width = useWindowSize().width / 12;
+    return (
+      <div className="container">
+        <Head>
+          <title>Super-code</title>
+          <link rel="icon" href="/favicon.ico" />
+          {/* <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-alpha.6/css/bootstrap.min.css" /> */}
+        </Head>
+        <main>
+          <HeaderWS width={width} />
+          {/* <Navigation /> */}
+          {/* <Header width={width} /> */}
+          {/* <HeaderCallToAction /> */}
+          {/* <Zukunft width={width} /> */}
+          {/* <UnserCampus width={width} /> */}
+          {/* <Weiterbildung /> */}
+          {/* <MeldeDich /> */}
+          {/* <Bewerbungsprozess /> */}
+          {/* <FAQ /> */}
+          {/* <FooterCallToAction month="Mai" /> */}
+          {/* <Footer /> */}
+        </main>
+
+        <style jsx>{``}</style>
+
+        <style jsx global>{`
+           @import url('https://fonts.googleapis.com/css?family=Fira+Mono:400,500,700|Fira+Sans:100,200,300,400,500,600,700,800,900&display=swap');        
+           html,
+           body {
+             padding: 0;
+             margin: 0;
+             font-family: 'Fira Sans', sans-serif;
+           }
+   
+           * {
+             box-sizing: border-box;
+           }
+           @media (max-width: 768px) {
+             body {
+               font-size: 10px;
+             }
+             }
+         }
+         `}</style>
+      </div>
+    );
+  } else {
+    return "";
+  }
+}
