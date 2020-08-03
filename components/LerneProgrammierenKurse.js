2f839e0e8a0ac5197b6a1c80f2881958aac74036
GFSTee <georg.treitz@gmail.com>
2020-08-03 09:38:04 +0200

    text changes

--- components/LerneProgrammierenKurse.js
+++ components/LerneProgrammierenKurse.js
@@ -15,7 +15,7 @@ const LerneProgrammierenKurse = () => {
         <Collapsible triggerTagName="h1" trigger="5 Monate Web-Developer Full-Stack " triggerExtra="Vollzeit 22 Wochen">
           <div className="kurse-infos">
             <div>
-              <p>Unser Full-Stack Kurs ermöglicht dir einen Web-Entwicklungs deep Dive verpackt in einem ultra praxisorientiertem instensiv Kurs. Lerne das Programmieren moderner Websites & Apps und finde einen Job mit Zukunft.</p>
+              <p>Unser Full-Stack-Kurs ermöglicht dir einen Web-Entwicklungs Deep Dive verpackt in einem ultra praxisorientiertem Intensiv-Kurs. Lerne das Programmieren moderner Websites & Apps und finde einen Job mit Zukunft.</p>
               <h3>Termine:</h3>
               <h4>18.05.20 - voll</h4>
               <h4>7.09.20 - Platz sichern⟶</h4>
@@ -50,7 +50,7 @@ const LerneProgrammierenKurse = () => {
         <Collapsible triggerTagName="h1" trigger="7 Monate Web-Developer Frond-End " triggerExtra="Teilzeit 30 Wochen">
           <div className="kurse-infos">
             <div>
-              <p>Programmieren lernen neben dem Beruf? Kein Problem! Unser Front-End Kurs findest abends und online statt! In 30 Wochen lernst du das Programmieren moderner Websites & Apps.</p>
+              <p>Programmieren lernen neben dem Beruf? Kein Problem! Unser Front-End Kurs findest abends und online statt! In 30 Wochen lernst du das Programmieren moderner Websites & Apps.</p>
               <h3>Termine:</h3>
               <h4>20.01.20 - voll</h4>
               <h4>7.09.20 - Platz sichern⟶</h4>
