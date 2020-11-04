aa9f952c94765d9bc75fb717ad1600fea21cd07b
GFSTee <georg.treitz@gmail.com>
2020-11-04 13:16:42 +0100

    blog hackathon added

--- components/data/blog.js
+++ components/data/blog.js
@@ -6,12 +6,15 @@ import blog3 from '../../markdown/blog3'
 import titleBlog3 from '../../markdown/titleBlog3'
 import blog4 from '../../markdown/blog4'
 import titleBlog4 from '../../markdown/titleBlog4'
+import blog5 from '../../markdown/blog5'
+import titleBlog5 from '../../markdown/titleBlog5'
+
 let blog = [
     { id: 0, title: titleBlog1, text: blog1, pic: '/img/blogs/1.jpg' },
     { id: 1, title: titleBlog2, text: blog2, pic: '/img/blogs/2.jpg' },
     { id: 2, title: titleBlog3, text: blog3, pic: '/img/blogs/3.jpg' },
     { id: 3, title: titleBlog4, text: blog4, pic: '/img/blogs/4.jpg' },
-    // { id: 4, title: titleBlog1, text: blog1 },
+    { id: 4, title: titleBlog5, text: blog5, pic: '/img/blogs/5.jpg' },
     // { id: 5, title: titleBlog1, text: blog1 },
 ]
 
