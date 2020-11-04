f917943be0ec0b56afb691287fc9f5465422f89e
GFSTee <georg.treitz@gmail.com>
2020-11-04 12:24:19 +0100

    alumni typo

--- markdown/titleBlog1.js
+++ markdown/titleBlog1.js
@@ -1,6 +1,5 @@
 const titleBlog1 = {
     main: `YouCode, YouTube, YouRock!”`,
     sub: 'Präsentation der Abschlussprojekte des 2. Bootcamps Webentwicklung Frontend als Youtube-Stream'
-
 }
 export default titleBlog1
