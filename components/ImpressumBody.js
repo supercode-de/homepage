bf2cd7d8cfde12b78ceddae53c383edd664ee8c2
GitM423 <matthias.p.hoeh@gmail.com>
2021-02-05 17:26:02 +0100

    impressum

--- components/ImpressumBody.js
+++ components/ImpressumBody.js
@@ -1,23 +1,30 @@
-
-
 const ImpressumBody = (props) => {
   return (
     <div id="impressum-text">
       <div className="text-body">
         <div className="block">
-          <p>Anbieter dieser Internetseiten ist dieSuperCode GmbH & Co. KG</p>
+          <p>Anbieter dieser Internetseiten ist die SuperCode GmbH & Co. KG</p>
           <p>hallo@super-code.de</p>
         </div>
         <div className="block">
           <p>Telefon: 0211 7817 2330</p>
         </div>
         <div className="block">
-          <p>Geschäftsführer: Sérgio Cardeal, Markus Lezaun</p>
+          <p>Geschäftsführer: Sérgio Cardeal</p>
           <p>Handelsregister Nummer: HRA 25439</p>
           <p>Registergericht: Amtsgericht Düsseldorf</p>
         </div>
         <div className="block">
-          <p>Alle Rechte an den Inhalten dieser Webseiten bleiben vorbehalten. Jede kommerzielle Verwertung bedarf der vorherigen Zustimmung des Rechtsinhabers. Diese Internetseite enthält gegebenenfalls Verknüpfungen (Links) zu Internetangeboten Dritter. Wir haben bei Setzen der Verknüpfung keine rechtswidrigen Inhalte des Verknüpfungsziels feststellen können. Für den aktuellen Inhalt und etwaige Änderungen nach Setzen der Verknüpfung können wir keine Gewähr übernehmen.</p>
+          <p>
+            Alle Rechte an den Inhalten dieser Webseiten bleiben vorbehalten.
+            Jede kommerzielle Verwertung bedarf der vorherigen Zustimmung des
+            Rechtsinhabers. Diese Internetseite enthält gegebenenfalls
+            Verknüpfungen (Links) zu Internetangeboten Dritter. Wir haben bei
+            Setzen der Verknüpfung keine rechtswidrigen Inhalte des
+            Verknüpfungsziels feststellen können. Für den aktuellen Inhalt und
+            etwaige Änderungen nach Setzen der Verknüpfung können wir keine
+            Gewähr übernehmen.
+          </p>
         </div>
       </div>
       <style jsx>{`
@@ -58,7 +65,6 @@ const ImpressumBody = (props) => {
         }
       `}</style>
     </div>
-
   );
 };
 
