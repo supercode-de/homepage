22078f0c868a910addd7ce27cf644cf35d004fb2
GFSTee <georg.treitz@gmail.com>
2021-02-17 12:00:32 +0100

    blog8

---
+++ markdown/blog8.js
@@ -0,0 +1,57 @@
+const blog8 = `# "Es sollte viel mehr Vorbilder geben, die zeigen, dass Coden nicht nur technisch, sondern auch unglaublich kreativ, kommunikativ und logisch ist."
+
+Ein Interview mit unserer Teilnehmerin Emilija Sekulic über ihre Gründe für einen Quereinstieg in die IT, ihre Erfahrungen im Bootcamp Webentwicklung und ihren Jobeinstieg als SuperWebentwicklerin. 
+
+![blog8](/img/blogs/8/1.jpg)
+
+_Hallo Emilija. Seit Januar arbeitest du als Junior Full-Stack-Entwicklerin in einer Werbeagentur in Köln. Wie fühlst du dich in deiner neuen Position?_
+
+Ich bin erleichtert und fühle mich unglaublich gut! Ich habe mir meinen Traum erfüllt und arbeite als Entwicklerin in Deutschland in einem netten Team. Ich bin jetzt seit drei Wochen angestellt und arbeite hauptsächlich mit dem MERN Stack, also MongoDB, Express, React und Node.js. Ich werde zukünftig Apps für die Kunden mit programmieren, da freue ich mich riesig drauf! 
+
+_Hast du das Team schon live kennenlernen können?_
+
+Momentan läuft alles online und auch das Vorstellungsgespräch war remote. Zum Glück war ich das Online-Arbeiten ja schon aus dem Bootcamp bei Supercode gewöhnt, daher war ich gut vorbereitet und konnte damit auch im Gespräch punkten. Ich freue mich aber sehr darauf, das ganze Team bald live kennenzulernen.
+
+_Wie lief denn die Jobsuche bei dir als Quereinsteigerin?_
+
+Es war für mich echt eine Überraschung so schnell etwas zu finden. Das integrierte Bewerbungscoaching hat mir sehr geholfen, da ich die Jobsuche in Deutschland nicht kannte. Ich habe gelernt, was in der Bewerbung wichtig ist und was die Arbeitgeber von mir erwarten. Mein Lebenslauf war dann am Ende vom Kurs fertig und damit konnte ich gut starten. Ich habe ein richtiges Upgrade von meinen Unterlagen gemacht und nach sechs Wochen intensiver Recherche und Bewerben hat es dann endlich geklappt! 
+
+_Wie konntest du im Vorstellungsgespräch überzeugen?_
+
+Bei der Agentur hatte ich insgesamt drei Gespräche und ein technisches Interview. Ich habe das Team, die Führungsabteilung und das Entwicklerteam kennengelernt und wir haben uns viel über meine Webprojekte und technische Grundlagen von React, Javascript und Co unterhalten. Ich war sehr ehrlich in den Gesprächen und habe meine Bereitschaft und Motivation zu lernen gezeigt. Nach der Zusage war ich unglaublich erleichtert! Ich habe nun einen Vollzeitjob als Entwicklerin, passe gut ins Team und fühle mich sehr gut.   
+
+_Du bist eigentlich studierte Journalistin. Wie kamst du zum Coden?_
+
+In Kroatien habe ich Journalistik studiert und fünf Jahre als Videojournalistin gearbeitet. Nebenbei hatte ich einen kleinen Online-Shop. Als ich dann mit meinem Partner nach Deutschland kam, habe ich schnell gemerkt, dass mein Englisch für die Medienbranche nicht ausreicht und ich schnell Deutsch lernen muss. Also habe ich einen Intensivkurs gemacht und im Einzelhandel gearbeitet. Ich habe viele Entwickler im Freundeskreis und fand das immer schon interessant. In Deutschland hatte ich dann endlich die Zeit, das mal auszuprobieren, habe mir ein Buch über die Programmiersprache Python geschnappt und mich als Hobby mit Funktionen und Variablen beschäftigt. Anfang 2020 habe ich dann den Entschluss gefasst, 100% Richtung Webentwicklung zu gehen und damit zukünftig mein Geld zu verdienen.  
+
+![blog8](/img/blogs/8/2.jpg)
+
+_Wie bist du dann auf SuperCode aufmerksam geworden und warum hast du dich für uns entschieden?_
+
+Da ich schnell in den Beruf einsteigen wollte, waren mir zwei Jahre Ausbildung zu lang und zu theoretisch. Also habe ich mich für das Bootcamp bei Supercode entschieden, weil es intensiv und schnell ist und wir viele Praxisprojekte machen. Das war ein großer Vorteil für mich und auch die Jobaussichten danach sahen gut aus. Ich wollte auch gerne mit anderen Leuten gemeinsam lernen, einen guten Rahmen haben und natürlich einen Nachweis am Ende bekommen. Außerdem wurde mir Supercode von einem Bekannten empfohlen. Für mich war das definitiv der richtige Weg!
+
+_Was waren deine Highlights im Kurs und was war herausfordernd für dich?_
+
+Das Beste war, jeden Tag zu coden und jeden Tag war etwas Neues zu lernen. Es gab viele schöne Projekte, die wir im Team bearbeitet haben. Alleine zu Hause hätte ich das nicht geschafft. Vor dem Bootcamp habe ich mich mit den Basics von HTML und CSS beschäftigt und hatte dadurch eine guten Einstieg. 
+
+Mein absolutes Lieblingsprojekt war mein Frontend-Abschlussprojekt, da das Team einfach super war. Alle hatten ein ernsthaftes Interesse daran, etwas Tolles auf die Beine zu stellen und alle haben sich eingebracht. Auch der interne Hackathon mit dem anderen Kurs war super, da wir mit fremden Leuten zusammen gearbeitet haben. Ich konnte meine Kenntnisse aus dem Backend-Bereich anwenden und Erfahrungen sammeln. Wer wirklich motiviert ist, Coden zu lernen und bereit ist seine Zeit für einen paar Monate zu investieren ist im Kurs genau richtig und hat auch gute Jobchancen!
+
+_Im IT-Bereich gibt es bisher nur 17-20% Frauen. War das ein Thema für dich?_
+
+Es gibt leider oft ein falsches Bild von Coder\*innen und dazu kommt, dass Mädchen weniger für Technik begeistert werden. Auch ich habe früher gedacht, dass der Bereich Coding sehr technisch und weniger interessant für mich ist. Dann habe ich mich mehr damit beschäftigt und gesehen, dass es um viel mehr geht! Ich konnte mir Vorbilder in meinem Freundeskreis suchen und wurde auch von Männern immer ermutigt, meinen Weg in die IT zu gehen. Trotzdem sollte es mehr weibliche Vorbilder geben, die zeigen, dass Coden nicht nur technisch, sondern auch unglaublich kreativ, kommunikativ und logisch ist.
+
+_Möchtest du anderen Quereinsteiger \*innen noch etwas mit auf den Weg geben?_
+
+Mein ganz praktischer Rat: Lerne jeden Tag Coden, finde eine\*n Mentor\*in oder Gruppe, die dich unterstützt und mach dir ein Bild deiner Zukunft als Entwickler\*in, das dich in schwierigen Momenten motiviert. Denn ein Karrierewechsel erfordert viel Mut und Durchhaltevermögen. Aber es lohnt sich definitiv! 
+
+Auch die Geschichten von anderen Menschen in ähnlichen Situationen können helfen.   
+Ich habe selbst viele Erfolgsstories von Karrierewechsler\*innen gelesen und habe gesehen, dass man es schaffen kann. Ich freue mich sehr, dass auch ich jetzt mit gutem Beispiel voran gehen kann und Frauen und Männer ermutige, den Schritt in die IT zu wagen. Dafür habe ich meine Erfahrungen auch in einem Blog festgehalten.   
+
+_Vielen Dank für das Interview Emilija und alles Gute für deine Zukunft als Junior Full-Stack-Developerin._
+
+![blog8](/img/blogs/8/3.png)
+
+_Das Interview führte Natalie Streich im Februar 2021 via Google-Meet._
+`;
+
+export default blog8;
---
+++ markdown/titleBlog8.js
@@ -0,0 +1,5 @@
+const titleBlog8 = {
+  main: `"Es sollte viel mehr Vorbilder geben, die zeigen, dass Coden nicht nur technisch, sondern auch unglaublich kreativ, kommunikativ und logisch ist."`,
+  sub: `Ein Interview mit unserer Teilnehmerin Emilija Sekulic über ihre Gründe für einen Quereinstieg in die IT, ihre Erfahrungen im Bootcamp Webentwicklung und ihren Jobeinstieg als SuperWebentwicklerin.`,
+};
+export default titleBlog8;
