b534dd980b3253580bf43264908b3f21e15f1d08
gfstee <georg.treitz@gmail.com>
2021-05-04 15:45:27 +0200

    Comments added

--- pages/blogs/[blogItem].js
+++ pages/blogs/[blogItem].js
@@ -6,39 +6,32 @@ import { useState, useEffect } from "react";
 import { useWindowSize } from "../../components/functions/windowSize";
 
 const blogItem = (props) => {
-
   const router = useRouter();
   const { blogItem } = router.query;
-
   const [blogs, setBlogs] = useState(BlogData);
   const [thisBlog, setThisBlog] = useState([])
 
   useEffect(() => {
     setBlogs(BlogData)
-    // console.log("mounted");
-    // console.log(blogs);
+    // Filtern die Daten nach der URL 
     setThisBlog(blogs.filter((singleBlog) => {
-      // console.log(singleBlog.title.main.replace(/ /g, "-") === blogItem)
       return singleBlog.title.main.replace(/ /g, "-") === blogItem
     }))
-    // console.log(thisBlog)
-    // thisBlog ? console.log(thisBlog[0].id) : console.log("error")
-
   }, [blogs, blogItem]);
-
-
-
 
   if (process.browser) {
     const width = useWindowSize().width / 12;
     return (
       <Layout>
+        {/* Erst wenn etwas in thisBlog ist geht es weiter */}
         {thisBlog.length !== 0 ? (
-          // JSON.stringify(thisBlog[0].id)
-          // // ""
           <BlogDetails
             width={width}
             title={blogs[thisBlog[0].id].title}
+            // TODO: Wie finden wir die nächste URL aus dem blogs 
+            // in thisBlog[0].id ist die aktuelle ID (das was bisher in unserem routerQuery / blogItem war)
+            // damit müssten wir weiter arbeiten können.
+
             // titleNext={
             //   blogItem == blogs.length - 1
             //     ? blogs[0].title
