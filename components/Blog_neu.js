30e70770031dba7d670b82ed2c56259ceaaa1c81
Finnschy <skylerfearless@yahoo.de>
2021-05-18 12:53:42 +0200

    neues faq item

--- components/Blog_neu.js
+++ components/Blog_neu.js
@@ -22,7 +22,7 @@ const responsive = {
     items: 1,
   },
 };
-let reveserBlog = blog.reverse();
+let reverseBlog = blog.reverse();
 
 const Blog_neu = (props) => {
   // const width = useWindowSize().width / 12
@@ -48,7 +48,7 @@ const Blog_neu = (props) => {
             deviceType={""}
             swipeable
           >
-            {reveserBlog.map((blogitem, i) => (
+            {reverseBlog.map((blogitem, i) => (
               <Link href={"/blogs/" + blogitem.title.main.replace(/ /g, "-").replace(/\?|\#|”|“|"|,|:|\./g, "")}>
                 <a target="_blank">
                   <div
--- pages/superblog.js
+++ pages/superblog.js
@@ -1,15 +1,20 @@
-export default function Superblog() {
-    if (process.browser) {
-      const width = useWindowSize().width / 12;
-      return (
-        <Layout>
-          <section className="blog-overview">
-            <h1>Blog</h1>
-          </section>
-        </Layout>
-      );
-    } else {
-      return "";
-    }
+import Super_Blog from "../components/Super_Blog";
+import { useWindowSize } from "../components/functions/windowSize";
+import Layout from "../components/Layout";
+
+export default function Superblog(props) {
+  if (process.browser) {
+    const width = useWindowSize().width / 12;
+    return (
+      <Layout banner={true}>
+        <div className="container">
+          <main>
+            <Super_Blog />
+          </main>
+        </div>
+      </Layout>
+    );
+  } else {
+    return "";
   }
-  
+}
