64d03833126c806c6fabdcb38834428e0103796f
couragesean <seancourage23@gmail.com>
2021-06-24 15:34:07 +0200

    react-gtm-module

--- pages/_app.js
+++ pages/_app.js
@@ -1,14 +1,22 @@
 import 'react-multi-carousel/lib/styles.css';
-import { library } from '@fortawesome/fontawesome-svg-core'
-import { faChevronDown } from '@fortawesome/free-solid-svg-icons'
-library.add(faChevronDown)
-import Router from "next/router";
+import { library } from '@fortawesome/fontawesome-svg-core';
+import { faChevronDown } from '@fortawesome/free-solid-svg-icons';
+import { useEffect } from 'react';
+import TagManager from 'react-gtm-module';
+library.add(faChevronDown);
+import Router from 'next/router';
 // import withFBQ from "next-fbq";
-import withAnalytics from "next-analytics";
+import withAnalytics from 'next-analytics';
 
 // This default export is required in a new `pages/_app.js` file.
 function MyApp({ Component, pageProps }) {
-    return <Component {...pageProps} />
+  useEffect(() => {
+    TagManager.initialize({ gtmId: 'GTM-5773QJD' });
+  }, []);
+  return <Component {...pageProps} />;
 }
 // export default withFBQ("353903998812058", Router)(MyApp);
-export default withAnalytics(Router, { ga: "UA-140157887-1", fbq: "353903998812058" })(MyApp);
+export default withAnalytics(Router, {
+  ga: 'UA-140157887-1',
+  fbq: '353903998812058',
+})(MyApp);
