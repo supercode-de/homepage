054a05af9045dd13ec3e23f6fbf55c6e11e09ec4
couragesean <seancourage23@gmail.com>
2021-08-06 14:19:35 +0200

    pages

--- components/KurseCard.js
+++ components/KurseCard.js
@@ -1,106 +1,121 @@
-import Curriculum from "./Curriculum_Popup";
-import Typeform_neu_small from "./TypeForm_neu_small";
+import Curriculum from './Curriculum_Popup';
+import Typeform_neu_small from './TypeForm_neu_small';
 const KurseCard = (props) => {
-    const { headline, umfang, start, copy, hubspotFormID, headlineID, typeformFormularID } = props.data
-    return (
-        <section className="kurseCard">
-            <div className="headlines">
-                <h5 id={headlineID} >{headline}</h5>
-            </div>
-            <div className="text">
-                <p>{umfang}</p>
-                <p>{start}</p>
-                <p>{copy}</p>
-            </div>
-            <div className="buttons">
-                <Typeform_neu_small typeformFormularID={typeformFormularID} />
-                <Curriculum buttonText="Curriculum" hubspot={hubspotFormID} />
-            </div>
-            <style jsx>{`
-                .kurseCard {
-                    border: 4px solid var(--super-green);
-                    background: var(--super-lila);
-                    white-space: pre-line;
-                    // display: flex;
-                    // flex-direction: column;
-                    // min-height: 100%;
-                    padding: 43px 28px;
-                    margin: 0 2rem;
+  const {
+    headline,
+    umfang,
+    start,
+    copy,
+    hubspotFormID,
+    headlineID,
+    typeformFormularID,
+  } = props.data;
+  return (
+    <section className='kurseCard'>
+      <div className='headlines'>
+        <h5 id={headlineID}>{headline}</h5>
+      </div>
+      <div className='text'>
+        <p>{umfang}</p>
+        <p>{start}</p>
+        <p>{copy}</p>
+      </div>
+      <div className='buttons'>
+        <Typeform_neu_small typeformFormularID={typeformFormularID} />
+        <Curriculum
+          buttonTextColor='#fff'
+          buttonText='Curriculum'
+          hubspot={hubspotFormID}
+        />
+      </div>
+      <style jsx>
+        {`
+          .kurseCard {
+            border: 4px solid var(--super-green);
+            background: var(--super-lila);
+            white-space: pre-line;
+            // display: flex;
+            // flex-direction: column;
+            // min-height: 100%;
+            padding: 43px 28px;
+            margin: 0 2rem;
 
-                    // diese grid Styles werden nur gebraucht, wenn das Carousel in Bootcamp.js da ist
-                    // das dient dazu, die Karten auf eine einheitliche Höhe zu bekommen.
-                    display: grid;
-                    grid-template-rows: 0.3fr 1.1fr minmax(20px, 60px);
-                    grid-template-areas:
-                    "headlines"
-                    "text"
-                    "buttons";                             
-                }
-                .headlines { grid-area: headlines; }
-                .text { grid-area: text; }
-                .buttons { grid-area: buttons; }
+            // diese grid Styles werden nur gebraucht, wenn das Carousel in Bootcamp.js da ist
+            // das dient dazu, die Karten auf eine einheitliche Höhe zu bekommen.
+            display: grid;
+            grid-template-rows: 0.3fr 1.1fr minmax(20px, 60px);
+            grid-template-areas:
+              'headlines'
+              'text'
+              'buttons';
+          }
+          .headlines {
+            grid-area: headlines;
+          }
+          .text {
+            grid-area: text;
+          }
+          .buttons {
+            grid-area: buttons;
+          }
 
-            
-                .kurseCard:hover {
-                    background: var(--super-blau);
-                }
-                .buttons {
-                    min-width: 100%;
-                    display: flex;                
-                    gap: 1rem;
-                }
-                h5 {
-                    font-family: "Neue_Machina_Ultrabold";
-                    font-size: 2.5rem;
-                    margin: 0;
-                    padding: 0;
-                }
-                p {
-                    font-family: "Neue_Machina_Regular_400";
-                    line-height: 25px;
-                }
+          .kurseCard:hover {
+            background: var(--super-blau);
+          }
+          .buttons {
+            min-width: 100%;
+            display: flex;
+            gap: 1rem;
+          }
+          h5 {
+            font-family: 'Neue_Machina_Ultrabold';
+            font-size: 2.5rem;
+            margin: 0;
+            padding: 0;
+          }
+          p {
+            font-family: 'Neue_Machina_Regular_400';
+            line-height: 25px;
+          }
 
-                @media (max-width: 1440px) {
-                    h5 {
-                        font-size: 2rem;
-                    }
-                    .kurseCard {
-                        grid-template-rows: 0.2fr 1.2fr minmax(20px, 60px);
-                    }
-                }
-                @media (max-width: 768px) {
-                    .kurseCard {
-                        // grid-template-rows: 0.2fr 1.2fr minmax(20px, 60px);
-                    }
-                    h5 {
-                        font-size: 1.6rem;
-                    }
-                    p {
-                        font-size: 14.7px;
-                    }                
-                }
-                @media (max-width: 425px) {
-                    .kurseCard {
-                        padding-bottom: 80px;
-                    }
-                    .buttons {
-                        flex-direction: column;
-
-                    }
-                }
-                @media (max-width: 375px) {
-                    h5 {
-                        font-size: 1.5rem;
-                    }
-                }
-                @media (max-width: 320px) {
-                    
-                }
-            `}
-
-            </style>
-        </section>
-    );
-}
+          @media (max-width: 1440px) {
+            h5 {
+              font-size: 2rem;
+            }
+            .kurseCard {
+              grid-template-rows: 0.2fr 1.2fr minmax(20px, 60px);
+            }
+          }
+          @media (max-width: 768px) {
+            .kurseCard {
+              // grid-template-rows: 0.2fr 1.2fr minmax(20px, 60px);
+            }
+            h5 {
+              font-size: 1.6rem;
+            }
+            p {
+              font-size: 14.7px;
+            }
+          }
+          @media (max-width: 425px) {
+            .kurseCard {
+              padding-bottom: 80px;
+            }
+            .buttons {
+              flex-direction: column;
+            }
+          }
+          @media (max-width: 375px) {
+            h5 {
+              font-size: 1.5rem;
+            }
+          }
+          @media (max-width: 320px) {
+          }
+        `}
+      </style>
+    </section>
+  );
+};
 
 export default KurseCard;
--- components/Navigation_neu.js
+++ components/Navigation_neu.js
@@ -1,8 +1,254 @@
+// import Link from 'next/link';
+// import React, { Component } from 'react';
+
+// class Navigation extends Component {
+//   state = { show: true, color: 'white', rotateDeg: 0 };
+//   listenScrollEvent = () => {
+//     let scrollLimit =
+//       Math.max(
+//         document.body.scrollHeight,
+//         document.body.offsetHeight,
+//         document.documentElement.clientHeight,
+//         document.documentElement.scrollHeight,
+//         document.documentElement.offsetHeight
+//       ) - document.documentElement.clientHeight;
+
+//     let numberOfRotations =
+//       Math.ceil(scrollLimit / document.documentElement.clientHeight / 3) * 0.5;
+//     let totalRotation = numberOfRotations * 360;
+
+//     this.setState({
+//       rotateDeg: (window.scrollY / scrollLimit) * totalRotation,
+//     });
+//     //   if (window.scrollY > 2700 && window.scrollY < 4700) {
+//     //     this.setState({ color: "black" });
+//     //   } else if (window.scrollY > 7100 && window.scrollY < 8670) {
+//     //     this.setState({ color: "black" });
+//     //   } else {
+//     //     this.setState({ color: "white" });
+//     //   }
+//   };
+
+//   componentDidMount() {
+//     window.addEventListener('scroll', this.listenScrollEvent);
+//   }
+
+//   render() {
+//     return (
+//       <nav
+//         id='navigation'
+//         style={{
+//           width: this.state.show ? '100%' : '5%',
+//         }}
+//       >
+//         <div className={`main-navigation ${this.state.show ? 'hide' : 'hide'}`}>
+//           <Link href='/'>
+//             <a className='logo'>
+//               <img
+//                 src='/img/Bildungsinstitut_Logo.svg'
+//                 alt='SuperCode-Logo'
+//                 className='rotate'
+//               />
+
+//               <img src='/img/SuperCode_Logo.svg' alt='SuperCode-Logo' />
+//             </a>
+//           </Link>
+
+//           <ul className='nav-list'>
+//             <li>
+//               <Link href='/kurse'>
+//                 <a>Kurse</a>
+//               </Link>
+//             </li>
+
+//             <li>
+//               <Link href='/workshops'>
+//                 <a>Workshops</a>
+//               </Link>
+//             </li>
+
+//             <li>
+//               <Link href='/team'>
+//                 <a>Team</a>
+//               </Link>
+//             </li>
+
+//             <li>
+//               <Link href='/#faq'>
+//                 <a>FAQ</a>
+//               </Link>
+//             </li>
+//             <li>
+//               <a onClick={this.props.toggleJetztAnmelden}>Kontakt</a>
+//             </li>
+//             <li>
+//               <Link href='/#blog'>
+//                 <a>Blog</a>
+//               </Link>
+//             </li>
+//           </ul>
+
+//           <img
+//             className='menu-button'
+//             src='/menu-button.svg'
+//             alt='Menu-Button'
+//             onClick={this.props.toggleNavigationMobile}
+//           />
+//         </div>
+
+//         <style jsx>{`
+//           ul {
+//             list-style-type: none;
+//             display: flex;
+//             // width: 100%;
+//             text-transform: uppercase;
+//             justify-content: flex-end;
+//             align-items: center;
+//             margin-right: 2em;
+//             padding-left: 0;
+//           }
+//           li {
+//             padding-left: 2em;
+//           }
+//           #navigation {
+//             // background: var(--super-black);
+//             display: flex;
+//             align-items: center;
+//             width: 100%;
+//             // height: 4vmax;
+//             transition: width 1s;
+//             position: relative;
+//             top: 40px;
+//             z-index: 999;
+//           }
+//           .logo {
+//             position: relative;
+//             height: 80px;
+//           }
+//           .logo img {
+//             height: 80px;
+//             width: 80px;
+//           }
+//           .logo .rotate {
+//             transform: rotate(${this.state.rotateDeg}deg);
+//           }
+
+//           .logo > img {
+//             position: absolute;
+//             top: 1px;
+//             left: -1px;
+//           }
+
+//           a {
+//             text-decoration: none;
+//             font-size: 1em;
+//             letter-spacing: 1.3px;
+//             color: #fff;
+//             letter-spacing: 2px;
+//             -webkit-text-stroke-width: 0.4px;
+//             -webkit-text-stroke-color: var(--super-black);
+//             transition: color 0.5s;
+//             cursor: pointer;
+//           }
+//           a:visited {
+//             color: #fff;
+//           }
+
+//           a:hover {
+//             // text-decoration: underline;
+//             color: #var(--super-green);
+//           }
+//           a:active {
+//             color: var(--super-lila);
+//           }
+
+//           .main-navigation {
+//             // background: var(--super-black);
+//             display: flex;
+//             justify-content: space-between;
+//             align-items: center;
+//             width: 100%;
+//             // box-sizing: border-box;
+//             transition: background 1s, right 1s;
+//             position: relative;
+//             padding: 0 25px;
+//             position: fixed;
+//             top: 5;
+//             z-index: 99;
+//           }
+//           .nav-list {
+//             display: ${window.scrollY > 0 ||
+//             document.documentElement.clientWidth < 768
+//               ? 'none'
+//               : 'flex'};
+//           }
+//           .menu-button {
+//             // position: fixed;
+//             // right: 2.5vw;
+//             height: 4vmax;
+//             min-height: 50px;
+//             z-index: 99998;
+//             cursor: pointer;
+//             display: ${window.scrollY > 0 ||
+//             document.documentElement.clientWidth < 768
+//               ? 'unset'
+//               : 'none'};
+//           }
+//           .logo {
+//             color: var(--super-green);
+//             // position: fixed;
+//             // left: 2.5vw;
+//             // padding-top: 4vh;
+//             // height: 4vmax;
+//             z-index: 99998;
+//           }
+//           .superCode-logo {
+//             z-index: 99998;
+//             cursor: pointer;
+//           }
+
+//           @media (max-width: 1040px) {
+//           }
+
+//           @media (max-width: 768px) {
+//           }
+
+//           @media (max-width: 520px) {
+//           }
+
+//           @media (max-width: 468px) {
+//           }
+//           @media (max-width: 400px) {
+//           }
+//           @media (max-width: 349px) {
+//           }
+//         `}</style>
+//       </nav>
+//     );
+//   }
+// }
+
+// export default Navigation;
+
 import Link from 'next/link';
 import React, { Component } from 'react';
 
 class Navigation extends Component {
-  state = { show: true, color: 'white', rotateDeg: 0 };
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      show: true,
+      color: 'white',
+      rotateDeg: 0,
+      showDropDown: false,
+    };
+    // this.wrapperRef = React.createRef();
+    // this.setWrapperRef = this.setWrapperRef.bind(this);
+    // this.handleClickOutside = this.handleClickOutside.bind(this);
+    this.handleClickOutside = this.handleClickOutside.bind(this);
+  }
+
   listenScrollEvent = () => {
     let scrollLimit =
       Math.max(
@@ -31,9 +277,22 @@ class Navigation extends Component {
 
   componentDidMount() {
     window.addEventListener('scroll', this.listenScrollEvent);
+    document.addEventListener('mousedown', this.handleClickOutside);
+  }
+  // componentDidMount() {
+  //   document.addEventListener('mousedown', this.handleClickOutside);
+  // }
+  handleClickOutside(event) {
+    if (event.target.id !== 'dropper') {
+      this.setState((currentState) => {
+        return { showDropDown: false };
+      });
+    }
   }
 
   render() {
+    const { dropdownColor } = this.props;
+
     return (
       <nav
         id='navigation'
@@ -55,10 +314,42 @@ class Navigation extends Component {
           </Link>
 
           <ul className='nav-list'>
-            <li>
+            <li
+              className='kurse-link'
+              onClick={() => this.setState({ showDropDown: true })}
+            >
               <Link href='/kurse'>
                 <a>Kurse</a>
               </Link>
+              {/* <div
+                className={
+                  this.state.showDropDown
+                    ? 'arrowWrapperNoAnimation'
+                    : 'arrowWrapper'
+                }
+              >
+                <img src='/img/navArrow.svg' alt='' />
+              </div> */}
+              {/* <div
+                // className='kurseList'
+                ref={this.wrapperRef}
+                id='dropper'
+                className={
+                  this.state.showDropDown
+                    ? `${'kurseList-open '}`
+                    : `${'kurseList '}`
+                }
+              >
+                <Link href='/fullstack'>
+                  <a id='dropper'>Fullstack Developer</a>
+                </Link>
+                <Link href='/frontend'>
+                  <a id='dropper'>Front-End Developer</a>
+                </Link>
+                <Link href='/uxui'>
+                  <a id='dropper'>UX & UI Designer</a>
+                </Link>
+              </div> */}
             </li>
 
             <li>
@@ -95,7 +386,6 @@ class Navigation extends Component {
             onClick={this.props.toggleNavigationMobile}
           />
         </div>
-
         <style jsx>{`
           ul {
             list-style-type: none;
@@ -121,6 +411,86 @@ class Navigation extends Component {
             top: 40px;
             z-index: 999;
           }
+
+          .arrowWrapper img {
+            width: 100%;
+          }
+
+          .kurse-link:hover > .arrowWrapper {
+            display: block;
+            width: 2.55%;
+            cursor: pointer;
+            padding-left: 20px;
+            animation: bounce 1.2s infinite;
+          }
+          .arrowWrapper {
+            position: absolute;
+            display: none;
+            width: 100%;
+            padding-left: 20px;
+            animation: bounce 1.2s infinite;
+          }
+          // .arrowWrapper:hover {
+          //   display: block;
+          //   width: 100%;
+          //   cursor: pointer;
+          //   padding-left: 20px;
+          //   animation: bounce 2s infinite;
+          // }
+          .arrowWrapperNoAnimation {
+            position: absolute;
+            display: block;
+            padding-left: 20px;
+            width: 2.55%;
+          }
+          .arrowWrapperNoAnimation img {
+            width: 100%;
+          }
+
+          .kurse-link:hover > .arrowWrapper {
+            display: block;
+          }
+
+          .kurseList-open {
+            position: absolute;
+            top: 80px;
+            height: 56px;
+            font-size: 1.2rem;
+
+            opacity: 1;
+            display: flex;
+            text-transform: none;
+            justify-content: space-around;
+            align-items: center;
+            padding-top: 2px;
+            background: ${this.props.dropdownColor
+              ? dropdownColor
+              : 'var(--super-green)'};
+            width: 100vw;
+            padding-left: 11%;
+            padding-right: 11%;
+            left: 0;
+
+            transition: height 0.4s, opacity 0.3s;
+          }
+
+          .kurseList {
+            position: absolute;
+            top: 46px;
+            height: 0px;
+            opacity: 0;
+            // transition: height 0.4s, opacity 0.3s;
+          }
+          .kurseList-open a {
+            -webkit-text-stroke-width: 1.5px;
+            -webkit-text-stroke-color: #fff;
+            font-size: 0.9em;
+          }
+
+          .kurseList-open a:hover {
+            text-decoration: underline;
+            text-decoration-thickness: 3px;
+          }
           .logo {
             position: relative;
             height: 80px;
@@ -143,9 +513,11 @@ class Navigation extends Component {
             text-decoration: none;
             font-size: 1em;
             letter-spacing: 1.3px;
-            color: #fff;
+            color: rgb(255, 255, 255);
             letter-spacing: 2px;
-            -webkit-text-stroke-width: 0.4px;
+            -webkit-text-stroke-width: ${this.props.navstroke
+              ? this.props.navstroke
+              : '0.4px'};
             -webkit-text-stroke-color: var(--super-black);
             transition: color 0.5s;
             cursor: pointer;
@@ -221,6 +593,21 @@ class Navigation extends Component {
           @media (max-width: 400px) {
           }
           @media (max-width: 349px) {
+          }
+          @keyframes bounce {
+            0%,
+            20%,
+            50%,
+            80%,
+            100% {
+              transform: translateY(0);
+            }
+            40% {
+              transform: translateY(-30px);
+            }
+            60% {
+              transform: translateY(-15px);
+            }
           }
         `}</style>
       </nav>
