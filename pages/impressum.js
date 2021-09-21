cdf3c26f238ac08207f841a2e6bf50e139e4f35c
Georg Treitz <georg.treitz@gmail.com>
2021-09-21 10:26:05 +0200

    impressum page width removed

--- pages/impressum.js
+++ pages/impressum.js
@@ -1,20 +1,15 @@
-import { useWindowSize } from '../components/functions/windowSize';
 import Layout from '../components/Layout';
 import ImpressumBody from '../components/ImpressumBody';
-import ImpressumHeader from '../components/ImpressumHeader';
 import Leitbild_Impressum_header from '../components/Leitbild_Impressum_Header';
 
 export default function Home(props) {
   if (process.browser) {
-    const width = useWindowSize().width / 12;
     return (
       <Layout banner={true}>
         <div className='container'>
           <main>
             <Leitbild_Impressum_header headline='Impressum' />
-            <ImpressumBody width={width} />
-            {/* <DsgvoHeader width={width} />
-            <DsgvoBody width={width} /> */}
+            <ImpressumBody />
           </main>
           <style jsx>{``}</style>
         </div>
