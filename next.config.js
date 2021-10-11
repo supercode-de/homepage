759d92d921bc6707e7ea229e26a4c89767b53490
Finnschy <skylerfearless@yahoo.de>
2021-10-11 18:50:16 +0200

    ich finde den fehler nicht aaaah

--- next.config.js
+++ next.config.js
@@ -26,33 +26,18 @@ module.exports = {
       '/blogs': { page: '/superblog' },
 
       '/blogs/YouCode-YouTube-YouRock!': { page: '/blogs/[blogItem]' },
-      '/blogs/Als-Web-Entwicklerin-werde-ich-mein-ganzes-Leben-lang-lernen---und-ich-habe-gelernt-wie-das-geht!':
-        { page: '/blogs/[blogItem]' },
+      '/blogs/Als-Web-Entwicklerin-werde-ich-mein-ganzes-Leben-lang-lernen---und-ich-habe-gelernt-wie-das-geht!': { page: '/blogs/[blogItem]' },
       '/blogs/Crazy-cool-people!': { page: '/blogs/[blogItem]' },
       '/blogs/SuperNatalie-goes-SuperCode': { page: '/blogs/[blogItem]' },
-      '/blogs/Hack-your-Life---1-SuperHackathon-bei-SuperCode-in-Düsseldorf': {
-        page: '/blogs/[blogItem]',
-      },
-      '/blogs/Lockdown-light-Bildung-goes-digital---again!': {
-        page: '/blogs/[blogItem]',
-      },
-      '/blogs/Cocktails-Curry-und-Kryptowährung---Halbzeit-beim-Fullstack-Bootcamp-Webentwicklung':
-        { page: '/blogs/[blogItem]' },
-      '/blogs/Coden-ist-nicht-nur-technisch-sondern-auch-unglaublich-kreativ-kommunikativ-und-logisch!':
-        { page: '/blogs/[blogItem]' },
+      '/blogs/Hack-your-Life---1-SuperHackathon-bei-SuperCode-in-Düsseldorf': { page: '/blogs/[blogItem]', },
+      '/blogs/Lockdown-light-Bildung-goes-digital---again!': { page: '/blogs/[blogItem]', },
+      '/blogs/Cocktails-Curry-und-Kryptowährung---Halbzeit-beim-Fullstack-Bootcamp-Webentwicklung': { page: '/blogs/[blogItem]' },
+      '/blogs/Coden-ist-nicht-nur-technisch-sondern-auch-unglaublich-kreativ-kommunikativ-und-logisch!': { page: '/blogs/[blogItem]' },
       '/blogs/Ist-das-Kunst-oder-kann-das-web': { page: '/blogs/[blogItem]' },
-      '/blogs/Wie-sich-die-IT-Branche-durch-Women-in-Tech-erneuern-lässt': {
-        page: '/blogs/[blogItem]',
-      },
-      '/blogs/Vom-Pfleger-zum-Webentwickler-in-sieben-Monaten': {
-        page: '/blogs/[blogItem]',
-      },
-      '/blogs/Mein-Herz-schlug-schon-immer-für-Design': {
-        page: '/blogs/[blogItem]',
-      },
-      'blogs/How-to-design-with-love-–-Der-neue-UX-UI-Design-Kurs-bei-SuperCode-zeigt-es-dir': {
-        page: '/blogs/[blogItem]',
-      }
+      '/blogs/Wie-sich-die-IT-Branche-durch-Women-in-Tech-erneuern-lässt': { page: '/blogs/[blogItem]', },
+      '/blogs/Vom-Pfleger-zum-Webentwickler-in-sieben-Monaten': { page: '/blogs/[blogItem]', },
+      '/blogs/Mein-Herz-schlug-schon-immer-für-Design': { page: '/blogs/[blogItem]', },
+      'blogs/How-to-design-with-love-–-Der-neue-UX-UI-Design-Kurs-bei-SuperCode-zeigt-es-dir': { page: '/blogs/[blogItem]', }
     };
   },
 };
