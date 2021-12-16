7fa3329a720625e4032c1e18230116a1d68b52dc
Franzi <71031130+franzockel@users.noreply.github.com>
2021-12-16 14:36:48 +0100

    Blogbeitrag Verlinkung funktionierte nicht

--- next.config.js
+++ next.config.js
@@ -4,40 +4,56 @@ module.exports = {
   },
   exportPathMap: function () {
     return {
-      '/': { page: '/' },
-      '/kurse': { page: '/kurse' },
-      '/workshops': { page: '/workshops' },
-      '/supercodeworkshop': { page: '/supercodeworkshop' },
-      '/beratung-coding': { page: '/beratungsformular-coding' },
-      '/beratungsformular': { page: '/beratungsformular-coding' },
-      '/beratung-design': { page: '/beratungsformular-design' },
-      '/dsgvo': { page: '/dsgvo' },
-      '/impressum': { page: '/impressum' },
-      '/team': { page: '/team' },
-      '/leitbild': { page: '/leitbild' },
+      "/": { page: "/" },
+      "/kurse": { page: "/kurse" },
+      "/workshops": { page: "/workshops" },
+      "/supercodeworkshop": { page: "/supercodeworkshop" },
+      "/beratung-coding": { page: "/beratungsformular-coding" },
+      "/beratungsformular": { page: "/beratungsformular-coding" },
+      "/beratung-design": { page: "/beratungsformular-design" },
+      "/dsgvo": { page: "/dsgvo" },
+      "/impressum": { page: "/impressum" },
+      "/team": { page: "/team" },
+      "/leitbild": { page: "/leitbild" },
       // '/blogs/0': { page: '/blogs/0', query: { title: '0' } },
-      '/404': { page: '/404' },
-      '/kurse/fullstack': { page: '/fullstack_kurs' },
-      '/kurse/frontend': { page: '/frontend-kurs' },
-      '/kurse/uxui': { page: '/ux-ui-kurs' },
-      '/supercodeworkshop': { page: '/supercodeworkshop' },
-      '/uxuidesignworkshop': { page: '/uxuidesignworkshop' },
-      '/danke': { page: '/danke' },
-      '/blogs': { page: '/superblog' },
+      "/404": { page: "/404" },
+      "/kurse/fullstack": { page: "/fullstack_kurs" },
+      "/kurse/frontend": { page: "/frontend-kurs" },
+      "/kurse/uxui": { page: "/ux-ui-kurs" },
+      "/supercodeworkshop": { page: "/supercodeworkshop" },
+      "/uxuidesignworkshop": { page: "/uxuidesignworkshop" },
+      "/danke": { page: "/danke" },
+      "/blogs": { page: "/superblog" },
 
-      '/blogs/YouCode-YouTube-YouRock!': { page: '/blogs/[blogItem]' },
-      '/blogs/Als-Web-Entwicklerin-werde-ich-mein-ganzes-Leben-lang-lernen---und-ich-habe-gelernt-wie-das-geht!': { page: '/blogs/[blogItem]' },
-      '/blogs/Crazy-cool-people!': { page: '/blogs/[blogItem]' },
-      '/blogs/SuperNatalie-goes-SuperCode': { page: '/blogs/[blogItem]' },
-      '/blogs/Hack-your-Life---1-SuperHackathon-bei-SuperCode-in-Düsseldorf': { page: '/blogs/[blogItem]', },
-      '/blogs/Lockdown-light-Bildung-goes-digital---again!': { page: '/blogs/[blogItem]', },
-      '/blogs/Cocktails-Curry-und-Kryptowährung---Halbzeit-beim-Fullstack-Bootcamp-Webentwicklung': { page: '/blogs/[blogItem]' },
-      '/blogs/Coden-ist-nicht-nur-technisch-sondern-auch-unglaublich-kreativ-kommunikativ-und-logisch!': { page: '/blogs/[blogItem]' },
-      '/blogs/Ist-das-Kunst-oder-kann-das-web': { page: '/blogs/[blogItem]' },
-      '/blogs/Wie-sich-die-IT-Branche-durch-Women-in-Tech-erneuern-lässt': { page: '/blogs/[blogItem]', },
-      '/blogs/Vom-Pfleger-zum-Webentwickler-in-sieben-Monaten': { page: '/blogs/[blogItem]', },
-      '/blogs/Mein-Herz-schlug-schon-immer-für-Design': { page: '/blogs/[blogItem]', },
-      '/blogs/How-to-design-with-love-–-Der-neue-UX-UI-Design-Kurs-bei-SuperCode-zeigt-es-dir': { page: '/blogs/[blogItem]', }
+      "/blogs/YouCode-YouTube-YouRock!": { page: "/blogs/[blogItem]" },
+      "/blogs/Als-Web-Entwicklerin-werde-ich-mein-ganzes-Leben-lang-lernen---und-ich-habe-gelernt-wie-das-geht!":
+        { page: "/blogs/[blogItem]" },
+      "/blogs/Crazy-cool-people!": { page: "/blogs/[blogItem]" },
+      "/blogs/SuperNatalie-goes-SuperCode": { page: "/blogs/[blogItem]" },
+      "/blogs/Hack-your-Life---1-SuperHackathon-bei-SuperCode-in-Düsseldorf": {
+        page: "/blogs/[blogItem]",
+      },
+      "/blogs/Lockdown-light-Bildung-goes-digital---again!": {
+        page: "/blogs/[blogItem]",
+      },
+      "/blogs/Cocktails-Curry-und-Kryptowährung---Halbzeit-beim-Fullstack-Bootcamp-Webentwicklung":
+        { page: "/blogs/[blogItem]" },
+      "/blogs/Coden-ist-nicht-nur-technisch-sondern-auch-unglaublich-kreativ-kommunikativ-und-logisch!":
+        { page: "/blogs/[blogItem]" },
+      "/blogs/Ist-das-Kunst-oder-kann-das-web": { page: "/blogs/[blogItem]" },
+      "/blogs/Wie-sich-die-IT-Branche-durch-Women-in-Tech-erneuern-lässt": {
+        page: "/blogs/[blogItem]",
+      },
+      "/blogs/Vom-Pfleger-zum-Webentwickler-in-sieben-Monaten": {
+        page: "/blogs/[blogItem]",
+      },
+      "/blogs/Mein-Herz-schlug-schon-immer-für-Design": {
+        page: "/blogs/[blogItem]",
+      },
+      "/blogs/How-to-design-with-love-–-Der-neue-UX-UI-Design-Kurs-bei-SuperCode-zeigt-es-dir":
+        { page: "/blogs/[blogItem]" },
+      "/blogs/https://www.super-code.de/blogs/IT-Frauen-WANTED!-Starte-2022-als-Webentwicklerin-mit-dem-SuperStipendium-Frauen-in-Tech!":
+        { page: "/blogs/[blogItem]" },
     };
   },
 };
