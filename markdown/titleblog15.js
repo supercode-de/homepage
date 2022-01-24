a320318bd52a372dacbdcab9a5cd00f042b0d658
SteffenKloempges <steffen.kloempges@gmail.com>
2022-01-24 16:51:23 +0100

    change titleBlog15.js

--- markdown/titleblog15.js
+++ markdown/titleblog15.js
@@ -1,5 +1,5 @@
 const titleBlog15 = {
-    main: `“Es ist einfach total schön, ein Teil der persönlichen Weiterentwicklung von Menschen zu sein. “`,
+    main: `“Es ist einfach total schön, ein Teil der persönlichen Weiterentwicklung von Menschen zu sein.“`,
     sub: `Interview mit unserem SuperTrainer Steffen über seine Coding-Anfänge, die Herausforderungen im Online-Bootcamp und seine Motivation, Menschen zu unterstützen.`,
 };
 export default titleBlog15;
