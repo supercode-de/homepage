5093c904a37e17e398a55736a91b3c4736837e70
Finnschy <skylerfearless@yahoo.de>
2022-02-16 12:24:30 +0100

    Blog Artikel 16 hinzugefügt

---
+++ markdown/titleBlog16.js
@@ -0,0 +1,5 @@
+const titleBlog15 = {
+    main: `“Ich bin dankbar, dass es Supercode gibt und sich Menschen die Zeit dafür nehmen, anderen etwas beizubringen.”`,
+    sub: `Interview mit unserem ehemaligen Teilnehmer Özcan über seinen erfolgreichen Quereinstieg in die IT nach dem Bootcamp Webentwicklung Full-Stack bei SuperCode in Düsseldorf.`,
+};
+export default titleBlog15;
