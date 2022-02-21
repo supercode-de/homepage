33fe220d161ef5ea10dabaded98b11eaf34bcd3d
Finnschy <skylerfearless@yahoo.de>
2022-02-21 11:55:45 +0100

    Blog16 Korrektur

--- markdown/blog16.js
+++ markdown/blog16.js
@@ -1,23 +1,23 @@
 const blog15 = `# “Ich bin dankbar, dass es Supercode gibt und sich Menschen die Zeit dafür nehmen, anderen etwas beizubringen.“ 
-*Interview mit unserem ehemaligen Teilnehmer Özcan über seinen erfolgreichen Quereinstieg in die IT nach dem Bootcamp Webentwicklung Full-Stack bei SuperCode in Düsseldorf.*
+ *Interview mit unserem ehemaligen Teilnehmer Özcan über seinen erfolgreichen Quereinstieg in die IT nach dem Bootcamp Webentwicklung Full-Stack bei SuperCode in Düsseldorf.* 
 
 ![blog15](/img/blogs/16/oezcan.png)
 
-“Özcan E., Ehemaliger Teilnehmer des Bootcamps, jetzt Webentwickler
+Özcan E., ehemaliger Teilnehmer des Bootcamps, jetzt Webentwickler
 
 ### Hallo Özcan. Gerade einmal einen Monat nach Abschluss des Bootcamps Webentwicklung Full-Stack hast du einen Job in der IT-Branche bekommen. Was machst du da genau?
 
-Ich bin angestellt als Junior Web- und Creation Manager und bin hauptsächlich in der Programmierung tätig. Dafür code ich sogenannte Module, Landingpages und Webseiten, die Nutzer*innen dazu einladen z.B. Whitepaper herunter zu laden oder ihre E-Mail-Adresse zu hinterlassen. Manchmal arbeite ich auch im Designbereich mit der Adobe Creative Cloud, da ich das als gelernter Mediengestalter natürlich auch kann. Mein Arbeitsplatz ist gut ausgestattet und wenn ich möchte kann ich auch hybrid also mal vor Ort, mal im Homeoffice arbeiten.  
+Ich bin angestellt als Junior Web- und Creation Manager und bin hauptsächlich in der Programmierung tätig. Dafür code ich sogenannte Module, Landingpages und Webseiten, die Nutzer * innen dazu einladen z.B. Whitepaper herunter zu laden oder ihre E-Mail-Adresse zu hinterlassen. Manchmal arbeite ich auch im Designbereich mit der Adobe Creative Cloud, da ich das als gelernter Mediengestalter natürlich auch kann. Mein Arbeitsplatz ist gut ausgestattet und wenn ich möchte kann ich auch hybrid also mal vor Ort, mal im Homeoffice arbeiten.  
 
-### Wie ist denn der Quereinstieg in die IT gelaufen? 
+### Wie ist denn der Quereinstieg in die IT gelaufen?  
 
-Als die Jobzusage kam, dachte ich einfach nur: “Endlich” Jetzt kann ich mal zeigen, was ich drauf habe! Insgesamt hatte ich dann doch einige Bewerbungen geschrieben, bis es geklappt hat. Nun bin erleichtert und glücklich, dass ich es so gut angetroffen habe.   
+Als die Jobzusage kam, dachte ich einfach nur: “Endlich!” Jetzt kann ich mal zeigen, was ich drauf habe! Insgesamt hatte ich dann doch einige Bewerbungen geschrieben, bis es geklappt hat. Nun bin erleichtert und glücklich, dass ich es so gut angetroffen habe.   
 ###
-Es läuft echt super! Mein Chef ist sehr zufrieden mit mir und meine Lernkurve ist wohl überdurchschnittlich hoch. Ich arbeite auch mit dem CRM Hubspot und bin dort in einer Community aktiv, in der Lösungen in Hinblick auf Programmierung und Design entwickelt werden. Dort bin ich sogar schon Champion geworden und habe von meinem Chef ein kleines Geschenk bekommen. Das ist natürlich eine schöne Anerkennung nach so kurzer Zeit.    
+Es läuft echt super! Mein Chef ist sehr zufrieden mit mir und meine Lernkurve ist wohl überdurchschnittlich hoch. Ich arbeite auch mit dem CRM Hubspot und bin dort in einer Community aktiv, in der Lösungen in Hinblick auf Programmierung und Design entwickelt werden. Dort bin ich sogar schon Champion geworden und habe von meinem Chef ein kleines Geschenk bekommen. Das ist natürlich eine schöne Anerkennung nach so kurzer Zeit.        
 
 ### Was hast du vorher gemacht und wie bist du dann zu Supercode gekommen?
 
-Ursprünglich habe ich Mediengestalter gelernt, konnte in der Branche aber nie so wirklich Fuß fassen. Daher habe ich auch eine Ausbildung zum Luftsicherheitsassistenten gemacht und am Flughafen Frankfurt gearbeitet. Freiberuflich war ich immer auch als Fotograf tätig. Dann habe ich mich über Weiterbildungen zum Webentwickler informiert und Supercode gefunden. Die Webseite wirkt moderner als bei anderen Bildungsträgern und ich hatte das Gefühl, dass sich bei Supercode wirklich um einen gekümmert wird. Es gibt höchstens 16 Leute in den Kursen und man lernt alles Wichtige, was man als Einsteiger*in braucht. Meine Trainer*innen haben sich immer gut gekümmert, wenn ich Hilfe brauchte.    
+Ursprünglich habe ich Mediengestalter gelernt, konnte in der Branche aber nie so wirklich Fuß fassen. Daher habe ich auch eine Ausbildung zum Luftsicherheitsassistenten gemacht und am Flughafen Frankfurt gearbeitet. Freiberuflich war ich immer auch als Fotograf tätig. Dann habe ich mich über Weiterbildungen zum Webentwickler informiert und Supercode gefunden. Die Webseite wirkt moderner als bei anderen Bildungsträgern und ich hatte das Gefühl, dass sich bei Supercode wirklich um einen gekümmert wird. Es gibt höchstens 16 Leute in den Kursen und man lernt alles Wichtige, was man als Einsteiger * in braucht. Meine Trainer * innen haben sich immer gut gekümmert, wenn ich Hilfe brauchte.    
 
 ### Was waren deine Highlights im Kurs und für was bist du dankbar? 
 
@@ -27,15 +27,15 @@ Ich bin echt dankbar für den Kurs bei Supercode. Bereits am Anfang wurde mir mi
 ###
 Super fand ich auch die Unterstützung im Bewerbungscoaching, bei dem wir coole Unterlagen erstellt haben und tolle Tipps bekommen haben. So konnte ich meine verschiedenen Berufsfelder gut verpacken und vermarkten und mit einer guten Bewerbung dann einen Job finden. 
 ###
-Ich bin dankbar, dass es sowas wie Supercode gibt und sich Menschen die Zeit dafür nehmen als Trainer*innen anderen etwas beizubringen. 
+Ich bin dankbar, dass es sowas wie Supercode gibt und sich Menschen die Zeit dafür nehmen als Trainer * innen anderen etwas beizubringen. 
 
 ### Was waren deine größten Herausforderungen im Kurs?
 
 Besonders am Ende, als wir den Backend-Teil gelernt haben und uns auch parallel um die Jobsuche gekümmert haben, hat mein Kopf manchmal einfach zu gemacht. Alles fühlte sich dann wie Kaugummi an. Dann darf man nicht den Kopf in den Sand stecken, sondern muss einfach weiter machen. Das Wichtigste dabei sind Geduld, Ruhe und Ausdauer. Man wird nicht von heute auf morgen zum Superbrain beim Coden und sollte seine Erwartungen anpassen. Es kommt immer wieder Neues dazu und man muss sich auch viel selbst aneignen. Das Trainerteam hat uns da super unterstützt und fast alle Fragen beantwortet. 
 
-### Was gibst du IT-Quereinsteiger*innen mit auf den Weg? 
+### Was gibst du IT-Quereinsteiger * innen mit auf den Weg? 
 
-Macht weiter mit dem Coding und bleibt dran! Nach dem Bootcamp hört das Lernen nicht auf und beim Jobeinstieg wird man mit viel Neuem konfrontiert. Wenn man aber die Logik des Codens einmal verstanden hat, kann man schnell umdenken und neues Wissen aufnehmen. Gut sind da natürlich Kontakte mit anderen Coder*innen, die einem helfen können. 
+Macht weiter mit dem Coding und bleibt dran! Nach dem Bootcamp hört das Lernen nicht auf und beim Jobeinstieg wird man mit viel Neuem konfrontiert. Wenn man aber die Logik des Codens einmal verstanden hat, kann man schnell umdenken und neues Wissen aufnehmen. Gut sind da natürlich Kontakte mit anderen Coder * innen, die einem helfen können. 
 ###
 Ganz wichtig sind auch eigene Webprojekte: Sei es eine Katzen-Webseite oder ein Schmuckblog für Halsketten der siebenjährigen Nichte. Hauptsache man macht weiter und wendet das Gelernte weiter an.    
 ###
@@ -44,7 +44,6 @@ Man kann Webentwicklung in 5 Monaten lernen, sollte sich aber nicht darauf ausru
 ### Vielen Dank für das Interview, Özcan und viel Freude in deinem neuen Job!
 
 Das Interview führte Natalie Streich im Dezember 2021 via google-meet
-Fotos: Supercode/privat
 
 `;
 
