e6cbaea0f2a2d9f6587b3730c33ec45fe98359f8
Finnschy <skylerfearless@yahoo.de>
2022-03-29 16:33:41 +0200

    problem: blog funktioniert lokal allerdings werden die artikel in netlify nicht angezeigt. ich weiß nicht wieso und habe daher noch keine lösung.

--- components/BlogDetails.js
+++ components/BlogDetails.js
@@ -3,7 +3,7 @@ import ReactMarkdown from "react-markdown";
 
 const BlogDetails = (props) => {
   const pixelSize = 2;
-  console.log("props", props);
+  console.log("props von blogdetails", props);
   return (
     <div id="blogDetails" className={`blackGitter ${props.klasse}`}>
       <section>
--- pages/blogs/[blogItem].js
+++ pages/blogs/[blogItem].js
@@ -17,7 +17,7 @@ const blogItem = (props) => {
       // console.log(singleBlog.title.main.replace(/ /g, "-") === blogItem)
 
       if (singleBlog.title.main.lastIndexOf('?' || '!') === -1) {
-        return singleBlog.title.main.replace(/ /g, "-").replace(/\?|\#|”|“|"|,|:|\./g, "") === blogItem                
+        return singleBlog.title.main.replace(/ /g, "-").replace(/\?|\#|”|“|"|,|:|\./g, "") === blogItem              
       } else {
         // blogItem does not continue after ?
         return singleBlog.title.main.replace(/ /g, "-").replace(/\?|\#|”|“|"|,|:|\./g, "").slice(0, singleBlog.title.main.indexOf('?')) === blogItem        
@@ -71,7 +71,7 @@ const blogItem = (props) => {
       </Layout>
     );
   } else {
-    return "";
+    return "This is not happening";
   }
 };
 
