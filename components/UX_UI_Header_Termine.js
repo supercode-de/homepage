93c4e29678e326e8da86f1952c579877cb7930e4
Finnschy <skylerfearless@yahoo.de>
2022-05-13 13:18:20 +0200

    Striche im Header weggenommen

--- components/UX_UI_Header_Termine.js
+++ components/UX_UI_Header_Termine.js
@@ -1,44 +1,45 @@
 const UXUIHeaderTermine = () => {
-    return (<div id="header-termine">
-        <div>
-            <div className="termine-wrapper">
-                <div className="start-termin">
-                    <img src="/img/FSHeader_Termine_Icon_UXUI.png" alt="" />
-                    <h2>18. Juli</h2>
+    return (
+        <div id="header-termine">
+            <div className="header-termine__flex">
+                <div>
+                    <div className="termine-wrapper">
+                        <div className="start-termin">
+                            <img src="/img/FSHeader_Termine_Icon_UXUI.png" alt="" />
+                            <h2>18. Juli</h2>
+                        </div>
+                    </div>
                 </div>
-                <div>Anmeldeschluss bis 15. Juli</div>
-            </div>
-        </div>
-        <div>
-            <div className="termine-wrapper">
-                <div className="start-termin">
-                    <img src="/img/FSHeader_Termine_Icon_UXUI.png" alt="" />
-                    <h2>26. September</h2>
+                <div>
+                    <div className="termine-wrapper">
+                        <div className="start-termin">
+                            <img src="/img/FSHeader_Termine_Icon_UXUI.png" alt="" />
+                            <h2>26. September</h2>
+                        </div>
+                    </div>
                 </div>
-                <div>Anmeldeschluss bis 23. September</div>
-            </div>
-        </div>
-        <div>
-            <div className="termine-wrapper">
-                <div className="start-termin">
-                    <img src="/img/FSHeader_Termine_Icon_UXUI.png" alt="" />
-                    <h2>21. November</h2>
+                <div>
+                    <div className="termine-wrapper">
+                        <div className="start-termin">
+                            <img src="/img/FSHeader_Termine_Icon_UXUI.png" alt="" />
+                            <h2>21. November</h2>
+                        </div>
+                    </div>
                 </div>
-                <div>Anmeldeschluss bis 18. November</div>
             </div>
-        </div>
-        <style jsx>
-            {`
+            <h2>Es sind keine Plätze mehr verfügbar.</h2>
+            <style jsx>{`
                 #header-termine{
                     padding: 10vh 0;
                     background: #42D8AC;
-                    display:flex;
-                    flex-wrap: wrap;
-                    justify-content: space-evenly;
                     // color: #ffda1a;
                     color: white;
                 }
-
+                .header-termine__flex {
+                    display:flex;
+                    flex-wrap: wrap;
+                    justify-content: space-evenly;
+                }
                 .termine-wrapper div:nth-child(2){
                     margin-top: 15px;
                 }
@@ -57,7 +58,12 @@ const UXUIHeaderTermine = () => {
                     padding-left: 5px;
                     font-size: 2.2rem;
                 }
-
+h2 {
+    text-align: center;
+    margin: 0;
+    margin-top: 50px;
+    font-size: 2em;
+}
                 @media (max-width: 1040px) {
                     .start-termin h2{
                         font-size: 1.8rem;
@@ -79,8 +85,8 @@ const UXUIHeaderTermine = () => {
                 }
 
             `}
-        </style>
-    </div>);
+            </style>
+        </div>);
 }
 
 export default UXUIHeaderTermine;
