bdf811c596459042a9636330d11d6d225a98009c
Finnschy <schlottmannfinn@gmail.com>
2022-08-30 17:55:39 +0200

    [kurseDetails] gefüllt weil netlify gemeckert hat

--- pages/microcamps/[kurseDetails].js
+++ pages/microcamps/[kurseDetails].js
@@ -1,11 +1,37 @@
+import Head from 'next/head';
 import LayoutMC from "../../components/LayoutMC";
 
-const kurseDetails = () => {
-    return (
-        <LayoutMC banner={true}>
-            <h1>Detailsseite</h1>
-        </LayoutMC>
-    );
-}
+import { useWindowSize } from "../../components/functions/windowSize";
+export default function kurseDetails() {
+    if (process.browser) {
+        const width = useWindowSize().width / 12;
+        return (
+            <LayoutMC banner={true}>
+                <h1>Kursedetails</h1>
+            </LayoutMC>
+        );
+    } else {
+        return (
+            <Head>
+                <meta
+                    property='og:image:secure'
+                    content='https://www.super-code.de/img/microcamps/people_center.png'
+                />
+                <meta
+                    property='og:image'
+                    content='http://www.super-code.de/img/microcamps/people_center.png'
+                />
+                <meta property='og:url' content='https://www.super-code.de/microcamps' />
+                <meta property='og:type' content='Website' />
+                <meta
+                    property='og:title'
+                    content='MicroCamps'
+                />
 
-export default kurseDetails;
+                <title>MicroCamps - Kursedetails</title>
+                <link rel='icon' type='image/svg+xml' href='/favicon.svg' />
+                <link rel='alternate icon' href='/favicon.ico' />
+            </Head>
+        );
+    }
+}
