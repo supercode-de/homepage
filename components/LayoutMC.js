508ad395be8179ce19f006ad953e828475cca822
Finnschy <schlottmannfinn@gmail.com>
2022-10-06 09:53:48 +0200

    neue machina schrift neu eingebunden weil fehlerhaft auf der microcamps seite

--- components/LayoutMC.js
+++ components/LayoutMC.js
@@ -132,15 +132,15 @@ class LayoutMC extends Component {
             @import url("https://fonts.googleapis.com/css2?family=Fira+Sans:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,100;1,200;1,300;1,400;1,500;1,600;1,700;1,800;1,900&display=swap");
             @font-face {
                 font-family: "NeueMachina-Light";
-                src: url("../fonts/NeueMachina-Light.otf") format("otf");
+                src: url('/Neue_Machina_Light.otf');
             }
             @font-face {
                 font-family: "NeueMachina-Regular";
-                src: url("../fonts/NeueMachina-Regular.otf") format("otf");
+                src: url('/Neue_Machina_Regular_400.otf');
             }
             @font-face {
                 font-family: "NeueMachina-Ultrabold";
-                src: url("../fonts/NeueMachina-Ultrabold.otf") format("otf");
+                src: url('/Neue_Machina_Ultrabold.otf');
             }
             :root {
                 // Colors
