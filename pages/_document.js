9cd694af8d5f2c0a06f8715da57822db0c9e79fb
Finnschy <schlottmannfinn@gmail.com>
2022-10-11 12:05:43 +0200

    netlify fehlermeldung versuchen zu fixen

--- pages/_document.js
+++ pages/_document.js
@@ -1,13 +1,15 @@
 import { Html, Head, Main, NextScript } from 'next/document'
-import Script from 'next/script'
+// import Script from 'next/script'
 
 class MyDocument extends Docuemnt {
     render() {
         return (
             <Html>
                 <Head>
-                    <Script src="https://static.clickskeks.at/18/d6/18d60b81-354e-4e9b-b119-078e881c7695/bundle.js" type="application/javascript" strategy="beforeInteractive"></Script>
-                    <Script src="https://static.clickskeks.at/18/d6/18d60b81-354e-4e9b-b119-078e881c7695/bundle.js" type="application/javascript" strategy="beforeInteractive"></Script>
+                    {/* <Script src="https://static.clickskeks.at/18/d6/18d60b81-354e-4e9b-b119-078e881c7695/bundle.js" type="application/javascript" strategy="beforeInteractive"></Script>
+                    <Script src="https://static.clickskeks.at/18/d6/18d60b81-354e-4e9b-b119-078e881c7695/bundle.js" type="application/javascript" strategy="beforeInteractive"></Script> */}
+                    <script src="https://static.clickskeks.at/18/d6/18d60b81-354e-4e9b-b119-078e881c7695/bundle.js" type="application/javascript" strategy="beforeInteractive"></script>
+                    <script src="https://static.clickskeks.at/18/d6/18d60b81-354e-4e9b-b119-078e881c7695/bundle.js" type="application/javascript" strategy="beforeInteractive"></script>
                 </Head>
                 <body>
                     <Main />
