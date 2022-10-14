fc21d4e504dd1ebfd58a5297241e9b29992fdc27
Finnschy <schlottmannfinn@gmail.com>
2022-10-14 14:02:33 +0200

    wartelisten popup gefixed

--- components/microcamps/MC_Warteliste_Popup.js
+++ components/microcamps/MC_Warteliste_Popup.js
@@ -1,8 +1,8 @@
+import { useState } from 'react';
 import Popup from 'reactjs-popup';
 import HubspotForm from 'react-hubspot-form';
 
 const Typeform = (props) => {
-  console.log(props.theme)
   return (
     <>
       <Popup
@@ -11,21 +11,21 @@ const Typeform = (props) => {
       >
         <div id='curriculum-popup'>
           <div className='curriculum-popup-content'>
+              <div className='curriculum-popup-input'>
+                <h1 className='three-lines'>
+                  Für die Warteliste <span className='super'>anmelden</span>
+                  <p>Unsere MicroCamps gehen 2023 an den Start. Wenn du das nicht verpassen willst, melde dich fix bei unserem Newsletter an! 😉</p>
+                </h1>
 
-            <div className='curriculum-popup-input'>
-              <h1 className='three-lines'>
-                Für die Warteliste <span className='super'>anmelden</span>
-                <p>Unsere MicroCamps gehen 2023 an den Start. Wenn du das nicht verpassen willst, melde dich fix bei unserem Newsletter an! 😉</p>
-              </h1>
-              <HubspotForm
-                portalId='5807040'
-                formId={props.wartelisteLinkFormID}
-                onSubmit={() => console.log('Submit!')}
-                onReady={(form) => console.log('Form ready!')}
-                loading={<div>Loading...</div>}
-                className='email-newsletter-form'
-              />
-            </div>
+                <HubspotForm
+                  portalId='5807040'
+                  formId={props.wartelisteLinkFormID}
+                  onSubmit={() => console.log('Submit!')}
+                  onReady={(form) => console.log('Form ready!')}
+                  loading={<div>Loading...</div>}
+                  className='email-newsletter-form'
+                />
+              </div>
           </div>
         </div>
       </Popup>
@@ -46,54 +46,23 @@ const Typeform = (props) => {
         .button:hover {
           opacity: 0.8;
         }
-
         #curriculum-popup {
           background: var(--super-black);
           color: var(--super-white);
-          padding: 54px;
+          padding: 5%;
           position: fixed;
-          left: 37%;
-          top: 11%;
-          width: 700px;
+          left: 50%;
+          top: 50%;
+          transform: translate(-50%,-50%);
+          width: 70%;
           color: var(--super-green);
-          z-index: 9999;
-        }
-        
-        .popup-close {
-          top: 0;
-          right: 0;
-          cursor: pointer;
-          position: absolute;
-          box-shadow: none;
-          border: none;
-          border-radius: 0;
-          padding: 0;
-          margin: 0;
-          color: var(--super-green);
-          background: none;
-          font-weight: 300;
-          font-size: 2em;
-          display: flex;
-          justify-content: center;
-          align-items: center;
-          height: 40px;
-          width: 40px;
-        }
-        .popup-close:hover {
-          color: var(--super-green);
+          z-index: 9;
         }
 
-        .curriculum-popup-image {
-          display: flex;
-        }
-        .curriculum-popup-image img {
-          width: 100%;
-          object-fit: cover;
-        }
         .curriculum-popup-input {
           width: 100%;
           overflow-y: hidden;
-          z-index:9999;
+          z-index: 9;
         }
         .curriculum-popup-input h1 {
           margin-bottom: 20px;
@@ -102,11 +71,6 @@ const Typeform = (props) => {
           color: var(--super-white);
           margin-top: 16px;
         }
-        iframe {
-          width: 100% !important;
-          box-sizing: border-box;
-        }
-
         h1 span.super {
           color: transparent;
           letter-spacing: 2px;
@@ -114,61 +78,16 @@ const Typeform = (props) => {
           -webkit-text-stroke-color: #fff;
           font-weight: 300;
         }
-        .one-line {
-          display: none;
-        }
 
-        @media (max-width: 1440px) {
-          .button {
-            font-size: 0.938em;
-          }
-          #curriculum-popup {
-          
-            left: ${(document.documentElement.clientWidth - 500) / 2}px;
-            width: 400px;
-          }
-
-        
-        }
         @media (max-width: 768px) {
-          #curriculum-popup {
-            // left: 50%;
-            // top: 50%;
-            left: ${(document.documentElement.clientWidth - 500) / 2}px;
-            width: 400px;
-          }
-          h1 {
-            font-size: 3.3em;
-          }
           .button {
             font-size: 1.1em;
           }
-          // .curriculum-popup-content {
-          //   grid-template-columns: 1fr;
-          // }
-
         }
         @media (max-width: 620px) {
-          #curriculum-popup {
-            width: calc(100vw / 12 * 9);
-            padding: 40px 0;
-          }
           .curriculum-popup-input {
             margin: auto;
             width: 90%;
-          }
-          .iframe {
-            // width: 80vw !important;
-          }
-        }
-
-        @media (max-width: 568px) {
-          #curriculum-popup {
-            left: ${(document.documentElement.clientWidth - 400) / 2}px;
-            width: 400px;
-          }
-          h1 {
-            font-size: 2.3em;
           }
         }
         @media (max-width: 500px) {
@@ -177,10 +96,6 @@ const Typeform = (props) => {
           }
         }
         @media (max-width: 468px) {
-          #curriculum-popup {
-            left: ${(document.documentElement.clientWidth - 300) / 2}px;
-            width: 300px;
-          }
           .button {
             font-size: 1.2em;
             width: 16em;
@@ -191,41 +106,8 @@ const Typeform = (props) => {
           .button {
             width: 100%;
           }
-        }
-        @media (max-width: 400px) {
-        }
-    
-        }
-        @media (max-height: ${
-          document.documentElement.clientHeight * 0.15 + 600
-        }px) {
-          .one-line {
-            display: block;
-          }
-          .three-lines {
-            display: none;
-          }
-          h1 {
-            font-family: 'Neue_Machina_Regular_400';
-            font-size: 2em;
-          }
-        }
-        @media (min-width: ${
-          document.documentElement.clientHeight / 2
-        }px) and (max-height: ${
-        document.documentElement.clientHeight * 0.15 + 600
-      }px) {
           #curriculum-popup {
-            width: ${
-              document.documentElement.clientWidth -
-              2 * (document.documentElement.clientWidth / 10)
-            }px;
-            left: ${
-              (document.documentElement.clientWidth -
-                (document.documentElement.clientWidth -
-                  2 * (document.documentElement.clientWidth / 10))) /
-              2
-            }px;
+            width: 90%;
           }
         }
       `}</style>
